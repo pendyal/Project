ede6165378ef345fcc8e63f496cde14e0b24dba8
aravind <aravind@gmail.com>
2024-12-12 20:12:28 -0500

    Final

--- reservations_project/settings.py
+++ reservations_project/settings.py
@@ -128,8 +128,8 @@ MEDIA_ROOT = Path(BASE_DIR) / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.outlook.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'manojbaws1@gmail.com'
-EMAIL_HOST_PASSWORD = 'Corei5@1'
+EMAIL_HOST_USER = 'pawsnclaws2003@gmail.com'
+EMAIL_HOST_PASSWORD = 'tinn wnsh gloj pete'
